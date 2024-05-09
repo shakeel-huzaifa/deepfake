80aee3b63d89c2c471917a6faa8c6aac0700875c
MAA99000 <maria.a.alawad@gmail.com>
2024-05-09 19:27:30 +0300

    Update apps.py

--- pred_app/apps.py
+++ pred_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
-from .utils.model_utils import load_resnet50_model, load_swin_transformer_model, load_EffNetB6_model
-#from .utils.model_utils import load_ininceptionv3_model
+from .utils.model_utils import load_resnet50_model, load_swin_transformer_model
+#from .utils.model_utils import load_EffNetB6_model
 
 
 class PredAppConfig(AppConfig):
@@ -10,5 +10,5 @@ class PredAppConfig(AppConfig):
     # Load the trained models
     resnet50 = load_resnet50_model()
     swin_transformer = load_swin_transformer_model()
-    effNetB6 = load_EffNetB6_model()
+    #effNetB6 = load_EffNetB6_model()
     # ininceptionv3_model =load_ininceptionv3_model()
